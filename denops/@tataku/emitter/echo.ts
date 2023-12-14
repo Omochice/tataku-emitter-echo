a55e0ac9e3f170925882f14f3b4ccf3ceedc2abf
Omochice <mochice.mls.ntl@gmail.com>
2023-12-14 11:19:53 +0900

    chore: migrate with stream api

--- denops/@tataku/emitter/echo.ts
+++ denops/@tataku/emitter/echo.ts
@@ -1,10 +1,12 @@
 import { Denops } from "https://deno.land/x/denops_std@v3.8.1/mod.ts";
 import { echo } from "https://deno.land/x/denops_std@v3.8.1/helper/mod.ts";
-import { Emitter } from "https://raw.githubusercontent.com/Omochice/tataku.vim/master/denops/tataku/interface.ts";
 
-export default class implements Emitter {
-  constructor() {}
-  async run(denops: Denops, source: string[]) {
-    await echo(denops, source);
-  }
-}
+const emitter = (denops: Denops, _option: unknown) => {
+  return new WritableStream<string[]>({
+    write: async (chunk: string[]) => {
+      await echo(denops, chunk.join("\n"));
+    },
+  });
+};
+
+export default emitter;
