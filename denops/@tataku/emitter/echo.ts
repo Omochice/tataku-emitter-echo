57d675074699c62d6e3456f2ea5f96396458fd99
Omochice <mochice.mls.ntl@gmail.com>
2024-08-03 14:49:10 +0900

    refactor: use emitterfactry type

--- denops/@tataku/emitter/echo.ts
+++ denops/@tataku/emitter/echo.ts
@@ -1,7 +1,8 @@
 import { Denops } from "jsr:@denops/std@7.0.2";
 import { echo } from "jsr:@denops/std@7.0.2/helper";
+import type { EmitterFactory } from "jsr:@omochice/tataku-vim@1.0.2";
 
-const emitter = (denops: Denops, _option: unknown) => {
+const emitter: EmitterFactory = (denops: Denops, _option: unknown) => {
   return new WritableStream<string[]>({
     write: async (chunk: string[]) => {
       await echo(denops, chunk.join("\n"));
